b02fa7ac77cfec0e893e7f17ee19dcdc79a746d0
Teodor <teodor1497@gmail.com>
2026-02-22 20:16:33 +0200

    Update Heading

--- src/Components/Heading/index.jsx
+++ src/Components/Heading/index.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import './Heading.css'
 
 
 const Heading = () => {
   return (
-    <div className='heading-container row row-cols-3'>
-        <div className='logo'>Logo</div>
-        <div className='categories  row row-cols-3'>
+    <div className='heading-container row row-cols-2'>
+        <div className='logo col-4'>Logo</div>
+        <div className='categories row row-cols-3'>
             <div className='category'>Category 1</div>
             <div className='category'>Category 2</div>
             <div className='category'>Category 3</div>
+            {/* <div className='category'>Category 1</div>
+            <div className='category'>Category 2</div>
+            <div className='category'>Category 3</div> */}
         </div>
     </div>
   )
