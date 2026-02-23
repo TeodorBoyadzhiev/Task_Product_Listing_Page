646129e9d353f5a9ad769d514606d43f1c1e80bb
Teodor <teodor1497@gmail.com>
2026-02-23 21:48:29 +0200

    Add load more button functionality

--- src/Components/PLP/Components/LoadMoreButton/index.jsx
+++ src/Components/PLP/Components/LoadMoreButton/index.jsx
@@ -1,8 +1,8 @@
 import './LoadMoreButton.css'
 
-const LoadMoreButton = () => {
+const LoadMoreButton = ({handleLoadMore}) => {
   return (
-    <div className="load-more-button btn btn-primary">
+    <div className="load-more-button btn btn-primary" onClick={handleLoadMore}>
       Load More
     </div>
   )
