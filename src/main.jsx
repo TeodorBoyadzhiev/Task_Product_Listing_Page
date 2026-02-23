c31dd9aea7995d1e1e1543239be65595606498d3
Teodor <teodor1497@gmail.com>
2026-02-23 22:50:21 +0200

    Redirect to category on initial load

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import './index.css'
 import App from './App.jsx'
 // routes
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Navigate to="/T-Shirts" replace />} />
       <Route path="/:category" element={<App />} />
     </Routes>
   </BrowserRouter>,
