768bab802b470c443112734567ffcbcf8b640002
Teodor <teodor1497@gmail.com>
2026-02-25 19:00:52 +0200

    Load more and footer styles

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import './Footer.css'
+// styles
+import './Footer.css';
+// router
+import { NavLink} from 'react-router';
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content container">
+        <div className="main-section row">
+          
+          <div className="brand-info col-md-4">
+            <h2 className="logo">Sweaty Betty</h2>
+            <p className="description">Premium quality for your daily style.</p>
+          </div>
 
-export default Footer
+          <div className="links-group col-md-4">
+            <div className="link-column">
+              <h3>Shop</h3>
+              <ul>
+                <li><NavLink to="/T-Shirts">T-Shirts</NavLink></li>
+                <li><NavLink to="/Jeans">Jeans</NavLink></li>
+                <li><NavLink to="/Sport-Shoes">Sport Shoes</NavLink></li>
+              </ul>
+            </div>
+            <div className="link-column">
+              <h3>Info</h3>
+              <ul>
+                <li><NavLink to="/shipping">Shipping</NavLink></li>
+                <li><NavLink to="/returns">Returns</NavLink></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="newsletter-section col-md-4">
+            <h3>Newsletter</h3>
+            <div className="input-group">
+              <input type="email" placeholder="Email" />
+              <button type="button">Sub</button>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="legal-bar">
+          <p>&copy; 2026 BRAND NAME. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Components/PLP/Components/LoadMoreButton/index.jsx
+++ src/Components/PLP/Components/LoadMoreButton/index.jsx
@@ -2,7 +2,7 @@ import './LoadMoreButton.css'
 
 const LoadMoreButton = ({handleLoadMore}) => {
   return (
-    <div className="load-more-button btn btn-primary" onClick={handleLoadMore}>
+    <div className="load-more-button btn btn-secondary btn-lg" onClick={handleLoadMore}>
       Load More
     </div>
   )
